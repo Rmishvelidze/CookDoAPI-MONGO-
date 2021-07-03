94f2d9029aa13c2db7e707831106ab33bdcb6171
Rezo Mishvelidze <rezo.mishvelidze@gmail.com>
2021-07-03 20:40:59 +0400

    Initiate commit and some templates

---
+++ CookDoAPIMongoDb/CookDo.Core/Models/Ingredient.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CookDo.Core.Models
+{
+    class Ingredient
+    {
+        [BsonId]
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
